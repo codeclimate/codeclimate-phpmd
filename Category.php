baef18b1e5fb4fda5f56b4a877389b390e57e930
Wilson Bilkovich <wilsonb@gmail.com>
2017-11-08 13:50:59 -0800

    Refactor repeated strings into constants, as per Sonar.

--- Category.php
+++ Category.php
@@ -1,45 +1,52 @@
 <?php
 namespace PHPMD\Category;
 
+const BUG_RISK = "Bug Risk";
+const CLARITY = "Clarity";
+const COMPATIBILITY = "Compatibility";
+const COMPLEXITY = "Complexity";
+const SECURITY = "Security";
+const STYLE = "Style";
+
 class Category
 {
     public static $checks = array(
-        "CleanCode/BooleanArgumentFlag" => ["Clarity", 300000],
-        "CleanCode/ElseExpression" => ["Clarity", 200000],
-        "CleanCode/StaticAccess" => ["Clarity", 200000],
-        "Controversial/CamelCaseClassName" => ["Style", 500000],
-        "Controversial/CamelCaseMethodName" => ["Style", 1000],
-        "Controversial/CamelCaseParameterName" => ["Style", 500000],
-        "Controversial/CamelCasePropertyName" => ["Style", 500000],
-        "Controversial/CamelCaseVariableName" => ["Style", 25000],
-        "Controversial/Superglobals" => ["Security", 100000],
-        "CyclomaticComplexity" => ["Complexity", 100000],
-        "Design/CouplingBetweenObjects" => ["Clarity", 400000],
-        "Design/DepthOfInheritance" => ["Clarity", 500000],
-        "Design/DevelopmentCodeFragment" => ["Security", 100000],
-        "Design/EvalExpression" => ["Security", 300000],
-        "Design/ExitExpression" => ["Bug Risk", 200000],
-        "Design/GotoStatement" => ["Clarity", 200000],
-        "Design/LongClass" => ["Complexity", 200000],
-        "Design/LongMethod" => ["Complexity", 200000],
-        "Design/LongParameterList" => ["Complexity", 200000],
-        "Design/NpathComplexity" => ["Complexity", 200000],
-        "Design/NumberOfChildren" => ["Clarity", 1000000],
-        "Design/TooManyFields" => ["Complexity", 900000],
-        "Design/TooManyMethods" => ["Complexity", 2000000],
-        "Design/TooManyPublicMethods" => ["Complexity", 2000000],
-        "Design/WeightedMethodCount" => ["Complexity", 2000000],
-        "ExcessivePublicCount" => ["Complexity", 700000],
-        "Naming/BooleanGetMethodName" => ["Style", 200000],
-        "Naming/ConstantNamingConventions" => ["Style", 100000],
-        "Naming/ConstructorWithNameAsEnclosingClass" => ["Compatibility", 400000],
-        "Naming/LongVariable" => ["Style", 1000000],
-        "Naming/ShortMethodName" => ["Style", 800000],
-        "Naming/ShortVariable" => ["Style", 500000],
-        "UnusedFormalParameter" => ["Bug Risk", 200000],
-        "UnusedLocalVariable" => ["Bug Risk", 200000],
-        "UnusedPrivateField" => ["Bug Risk", 200000],
-        "UnusedPrivateMethod" => ["Bug Risk", 200000],
+        "CleanCode/BooleanArgumentFlag" => [CLARITY, 300000],
+        "CleanCode/ElseExpression" => [CLARITY, 200000],
+        "CleanCode/StaticAccess" => [CLARITY, 200000],
+        "Controversial/CamelCaseClassName" => [STYLE, 500000],
+        "Controversial/CamelCaseMethodName" => [STYLE, 1000],
+        "Controversial/CamelCaseParameterName" => [STYLE, 500000],
+        "Controversial/CamelCasePropertyName" => [STYLE, 500000],
+        "Controversial/CamelCaseVariableName" => [STYLE, 25000],
+        "Controversial/Superglobals" => [SECURITY, 100000],
+        "CyclomaticComplexity" => [COMPLEXITY, 100000],
+        "Design/CouplingBetweenObjects" => [CLARITY, 400000],
+        "Design/DepthOfInheritance" => [CLARITY, 500000],
+        "Design/DevelopmentCodeFragment" => [SECURITY, 100000],
+        "Design/EvalExpression" => [SECURITY, 300000],
+        "Design/ExitExpression" => [BUG_RISK, 200000],
+        "Design/GotoStatement" => [CLARITY, 200000],
+        "Design/LongClass" => [COMPLEXITY, 200000],
+        "Design/LongMethod" => [COMPLEXITY, 200000],
+        "Design/LongParameterList" => [COMPLEXITY, 200000],
+        "Design/NpathComplexity" => [COMPLEXITY, 200000],
+        "Design/NumberOfChildren" => [CLARITY, 1000000],
+        "Design/TooManyFields" => [COMPLEXITY, 900000],
+        "Design/TooManyMethods" => [COMPLEXITY, 2000000],
+        "Design/TooManyPublicMethods" => [COMPLEXITY, 2000000],
+        "Design/WeightedMethodCount" => [COMPLEXITY, 2000000],
+        "ExcessivePublicCount" => [COMPLEXITY, 700000],
+        "Naming/BooleanGetMethodName" => [STYLE, 200000],
+        "Naming/ConstantNamingConventions" => [STYLE, 100000],
+        "Naming/ConstructorWithNameAsEnclosingClass" => [COMPATIBILITY, 400000],
+        "Naming/LongVariable" => [STYLE, 1000000],
+        "Naming/ShortMethodName" => [STYLE, 800000],
+        "Naming/ShortVariable" => [STYLE, 500000],
+        "UnusedFormalParameter" => [BUG_RISK, 200000],
+        "UnusedLocalVariable" => [BUG_RISK, 200000],
+        "UnusedPrivateField" => [BUG_RISK, 200000],
+        "UnusedPrivateMethod" => [BUG_RISK, 200000],
     );
 
     public static function pointsFor($checkName, $metric)
